1d15d507bb5c3a519302fa3a5f25be4c6082948b
wangfei <wangfei@zhihu.com>
2018-11-02 11:44:03 +0800

    0.1.1 bug fix

--- lib/hemera/source/xcode.rb
+++ lib/hemera/source/xcode.rb
@@ -6,17 +6,19 @@ module Hemera
       xcodeproj_relative_path = "#{path}/*.xcodeproj" unless Dir.glob(xcodeproj_relative_path).any?
       system "open #{xcodeproj_relative_path}"
     end
+
     def self.clean
+      puts 'In order to clean complete，xcode will be quit'
+      `osascript -e 'quit app "Xcode"'`
       path_command = 'defaults read ~/Library/Preferences/com.apple.dt.Xcode.plist IDECustomDerivedDataLocation'
-      Open3.popen3(path_command) { |_, stdout, _, _|
-        path = stdout.read.to_s.split("\n")[0]
+      Open3.popen3 path_command do |_, stdout, _, wait_thr|
+        path = wait_thr.value.success? ? stdout.read.to_s.split("\n")[0] : '~/Library/Developer/Xcode/DerivedData'
         func = 'rm -rf'
-        path = '~/Library/Developer/Xcode/DerivedData' unless File.exist? path
         command = "#{func} #{path}"
-        puts "#{path} Cleaning"
+        puts "#{path} cleaning, wait a moment please 😄"
         system command
-        puts "Clean Complete"
-      }
+        puts "Clean complete, 👍"
+      end
     end
   end
 end
