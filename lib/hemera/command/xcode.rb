28b944a981454ed6925f8daae55bc3b9b6809ec9
liubo <liubo004@126.com>
2018-11-15 19:45:06 +0800

    [IMP] 添加 hm xcode open 默认参数

--- lib/hemera/command/xcode.rb
+++ lib/hemera/command/xcode.rb
@@ -4,8 +4,8 @@ desc 'Xcode'
 command :xcode, :x do |c|
   c.desc 'Open Xcode Workspace'
   c.command :o, :open do |com|
-    com.action do |_global_options, _options, _args|
-      Hemera::Xcode.open _args.first
+    com.action do |_global_options, _options, args|
+      Hemera::Xcode.open args.first || '.'
     end
   end
 
